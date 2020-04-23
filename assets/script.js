81f3f75ca6a7a1c56ebd7b57f379568c3ab5e435
Zerrtry <Zerrtry@icloud.com>
2020-04-22 23:43:06 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,68 @@
-// Assignment Code
+// Assignment Code:
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Here we prompt and store a length of the password:
+function getNumberOfCharacters() {
+  var userInputOfLength = prompt("Please choose a length of at least 8 characters and no more than 128 characters.",0);
+  //check if password is long enough
+  for (var i = 0; i < Infinity; i++) {
+    if (isNaN(userInputOfLength) || userInputOfLength <8 || userInputOfLength >128) {
+      var userInputOfLength = prompt("Please try again, choose a length of at least 8 characters and no more than 128 characters.");
+    } else {
+      console.log("password length:", userInputOfLength);
+      return userInputOfLength;
+      break;
+    };
+  };
+};
+
+// Here we confirm and store wich types of characters should be included in password:
+function getKindsOfCharacters() {
+  var specialCharactersString = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  var lowercaseCharactersString = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseCharactersString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericCharactersString = "0123456789";
+  var stringOfChosenCharacters;
+  var specialCharactersOption = confirm("Press OK, if you'd like to include special characters in the password.");
+  if (specialCharactersOption == true) {
+    stringOfChosenCharacters = specialCharactersString;
+  }; 
+  var lowercaseCharactersOption = confirm("Press OK, if you'd like to include lowercase in the password.");
+  if (lowercaseCharactersOption == true) {
+    stringOfChosenCharacters = stringOfChosenCharacters + lowercaseCharactersString;
+  };
+  var uppercaseCharactersOption = confirm("Press OK, if you'd like to include uppercase in the password.");
+  if (uppercaseCharactersOption == true) {
+    stringOfChosenCharacters = stringOfChosenCharacters + uppercaseCharactersString;
+  };
+  var numericCharactersOption = confirm("Press OK, if you'd like to include numbers in the password.");
+  if (numericCharactersOption == true) {
+    stringOfChosenCharacters = stringOfChosenCharacters + numericCharactersString;
+  };
+  //check if user choose at least one kind of character:
+  if (!specialCharactersOption && !lowercaseCharactersOption && !uppercaseCharactersOption && !numericCharactersOption) {
+    var userInputOfLength = alert("You did choose no one kind of character. Please refresh and try again.");
+  } else {
+    console.log("picked characters: ", stringOfChosenCharacters);
+    return stringOfChosenCharacters;
+  };
+};
+
+// Password generator function
+function generatePassword() {
+  var numberOfCharacters = getNumberOfCharacters();
+  var pickedKindsOfCharacters = getKindsOfCharacters();
+  var passwordItems = "";
+  for (var i = 0; i < numberOfCharacters; i++) {
+    passwordItems = passwordItems + pickedKindsOfCharacters.charAt(Math.floor(Math.random() * Math.floor((pickedKindsOfCharacters.length) - 1)));
+  }; 
+    console.log("password: ", passwordItems);
+    return passwordItems;
+};
+
+// Write password to the #password input:
 function writePassword() {
+  console.log ('writePassword invoked');
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,5 +70,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button:
 generateBtn.addEventListener("click", writePassword);
